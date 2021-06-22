2cfc05828d1ebe1a45b625d4725d2a41b56db402
Ben Fox <bfox11677@gmail.com>
2021-06-22 18:19:37 -0500

    last

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  const books = Book.findAll();
+  console.log(books.map(book => book.toJSON()));
+  console.log(books);
 });
+
+//   let books;
+  
+//   (async () => {
+  
+//     try {
+  
+//       // ... All model instances
+//       books = await Book.findAll();
+//       console.log( books.map(book => book.toJSON()) );
+//       res.render('index', books);
+//     } catch(error) {
+      
+//     }
+//   })();
+// });
+    
+
 
 module.exports = router;
